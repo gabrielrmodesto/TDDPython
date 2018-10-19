0fd6f8a62e12bb0ce514f4b58c2e76e7a7978aa5
GabrielRM <gabrielrmodesto5@gmail.com>
2018-10-19 09:58:10 -0300

    teste em lista

--- codigo.py
+++ codigo.py
@@ -1,2 +1,9 @@
 def hello_world():
     return 'Hello World'
+
+def cria_lista(tam):
+    lista = []
+    for i in range(10):
+        lista.append(i)
+
+    return lista
--- teste.py
+++ teste.py
@@ -5,5 +5,8 @@ class ClasseDeTeste(unittest.TestCase):
     def test_hello(self):
         self.assertEqual(hello_world(),'Hello World')
 
+    def teste_lista(self):
+        self.assertEqual(10, len(cria_lista(10)))
+
 if __name__ == '__main__':
     unittest.main()
