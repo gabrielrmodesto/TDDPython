c0d0bdaf3a85922d900c5d33dacf02b2dc3aa583
GabrielRM <gabrielrmodesto5@gmail.com>
2018-10-19 09:37:41 -0300

    Teste retornado com certeza

--- teste.py
+++ teste.py
@@ -2,4 +2,8 @@ import unittest
 from codigo import *
 
 class ClasseDeTeste(unittest.TestCase):
-    
+    def test_hello(self):
+        self.assertEqual(hello_world(),'Hello World')
+
+if __name__ == '__main__':
+    unittest.main()
